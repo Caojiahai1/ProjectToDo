6d47e01a4a6059bc51fd5732ff7ac41904cf2fa9
wchenchen30 <wchenchen30@users.noreply.github.com>
2019-10-10 17:07:13 +0800

    添加事项

--- pages/index/index.js
+++ pages/index/index.js
@@ -43,7 +43,9 @@ Page({
     // taskBgColor: '#6491CB',
     inputBottom: 0,
     inputDisplay: "none",
-    inputShowed: false
+    inputShowed: false,
+    TodayList:[],
+    input:""
   },
 
   addTask: function() {
@@ -179,9 +181,70 @@ Page({
     var date2 = new Array("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六");
     var Today;
     Today = date1.getFullYear() + '-' + (date1.getMonth() + 1) + '-' + date1.getDate() + '   ' + (date2[date1.getDay()]);
-
     that.setData({
       searchplaceholder: Today
     });
-  }
+  },
+
+
+// 新增
+  save: function () {
+    wx.setStorageSync('TodayList', this.data.TodayList);
+  },
+
+  loadData: function () {
+    var todo = wx.getStorageSync('TodayList');
+    if (todo) {
+      this.setData({
+        TodayList: todo
+      });
+    }
+  },
+
+  AddInput: function (e) {
+    this.setData({
+      input: e.detail.value
+    });
+  },
+
+  //更改任务状态
+  toggleTodoHandle: function (e) {
+    var todo = this.data.TodayList;
+    //获取e传输的id
+    var index = e.currentTarget.id;
+    //改变complete状态
+    todo[index].completed = !todo[index].completed;
+    //更改数据
+    this.setData({
+      TodayList:todo
+    });
+    this.save();
+  },
+
+// 增加一条记录
+
+  AddConfirm: function (e) {
+    var that = this;
+    var todo = this.data.TodayList;
+    todo.push({ description: this.data.input, completed: false })
+    //更新数据
+    that.setData({ TodayList: todo, input: '' });
+    //输出日志信息
+    console.log(this.data.TodayList)
+    //保存记录到本地
+  },
+  // 清除一条记录
+  removeTodoHandle: function (e) {
+    var todo = this.data.TodayList;
+    var index = e.currentTarget.id;
+    //删除数据
+    todo.splice(index, 1);
+    console.log(todo);
+    //设置数据
+    this.setData({
+      TodayList: todo
+    });
+    this.save();
+  },
+
 })
